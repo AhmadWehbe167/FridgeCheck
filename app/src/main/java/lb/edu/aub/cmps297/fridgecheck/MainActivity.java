a69756ceca3533c74dea866cf7dfa11e0936fa07
AhmadWehbeStudent <ahmadbilalwehbe1234@gmail.com>
2021-10-30 00:01:02 +0300

    Initial commit

---
+++ app/src/main/java/lb/edu/aub/cmps297/fridgecheck/MainActivity.java
@@ -0,0 +1,20 @@
+package lb.edu.aub.cmps297.fridgecheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+}
