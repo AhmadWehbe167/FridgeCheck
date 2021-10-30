e7b14890d2ade53bc3c652c288c570e0a0faf29d
AhmadWehbeStudent <ahmadbilalwehbe1234@gmail.com>
2021-10-30 14:33:21 +0300

    NavigationBar

--- app/src/main/java/lb/edu/aub/cmps297/fridgecheck/MainActivity.java
+++ app/src/main/java/lb/edu/aub/cmps297/fridgecheck/MainActivity.java
@@ -1,6 +1,9 @@
 package lb.edu.aub.cmps297.fridgecheck;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,12 +12,21 @@ import android.view.MenuItem;
 import android.widget.SearchView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        //setup for navigation bar
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navController = Navigation.findNavController(this, R.id.fragment2);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
