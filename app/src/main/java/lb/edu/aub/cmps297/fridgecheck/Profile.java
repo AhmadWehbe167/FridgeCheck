54c2ef2ad83026f96c2c44ca56415b82a6afcb1c
Omar Kammouni <omk12@mail.aub.edu>
2021-11-27 14:51:33 +0200

    fixed profile, name didn't work yet

--- app/src/main/java/lb/edu/aub/cmps297/fridgecheck/Profile.java
+++ app/src/main/java/lb/edu/aub/cmps297/fridgecheck/Profile.java
@@ -1,28 +1,60 @@
 package lb.edu.aub.cmps297.fridgecheck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Profile extends AppCompatActivity {
 
     private ImageView back;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_profile);
-        back = findViewById(R.id.imageView12);
+        /*back = findViewById(R.id.imageView12);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openBack();
             }
-        });
+        });*/
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String id = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String userEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        TextView displayName = (TextView) findViewById(R.id.textView10);
+        TextView displayEmail = (TextView) findViewById(R.id.textView9);
+        displayEmail.setText(userEmail);
+
+/*        db.collection("Users").whereEqualTo("uId",id).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DocumentSnapshot document : task.getResult())
+                        displayName.setText((CharSequence) document.get("Name"));
+            }
+        }});
+        System.out.println(displayName);*/
     }
     public void openBack(){
         Intent intent = new Intent(this, SettingsFragment.class);
